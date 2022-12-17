43d9c6f1d5ac58ba83f486e026e8bd5b7aeae6b5
coach77777 <108881946+coach77777@users.noreply.github.com>
2022-12-17 14:06:44 -0600

    Options API

--- vue-composition-api-basics/src/views/HomeView.vue
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -1,8 +1,53 @@
 
-
+<!-- Template Sxn************* -->
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div>
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button @click="increaseCounter" class="btn">+</button>
+    </div>
   </div>
-
 </template>
+<!-- Template Sxn************* -->
+
+
+<!--  script sxn -->
+<script>
+
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+// Methods
+  methods: {
+
+   // add to the the Counter
+    increaseCounter() {
+      this.counter++
+    },
+
+     // minus the Counter
+    decreaseCounter() {
+      this.counter--
+    }
+  }
+}
+</script>
+
+
+<!-- ************* StylesSxn************* -->
+<style>
+.home {
+  text-align: center;
+  padding: 20 px;
+}
+
+.btn,
+.counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
