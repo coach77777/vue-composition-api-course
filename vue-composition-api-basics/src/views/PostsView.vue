d5f221f9bbd55ea5e8714396b6a8b21b0b2aeda9
coach77777 <108881946+coach77777@users.noreply.github.com>
2022-12-20 13:25:53 -0600

    VueRouterSxn

---
+++ vue-composition-api-basics/src/views/PostsView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="posts">
+    <h1>Posts</h1>
+<ul>
+  <li>
+    <RouterLink to="postDetail/id1">Post1
+  </RouterLink>
+</li>
+  <li>
+    <RouterLink to="postDetail/id2">Post2
+  </RouterLink>
+</li>
+  <li>
+    <RouterLink to="postDetail/id3">Post3
+  </RouterLink>
+</li>
+</ul>
+
+    <textarea v-autofocus />
+  </div>
+
+</template>
+
+<script setup>
+/*
+  imports
+*/
+  import { vAutofocus } from '@/directives/vAutofocus'
+</script>
+
+<style scoped>
+ul {
+  margin-bottom: 30px;
+}
+</style>
+
