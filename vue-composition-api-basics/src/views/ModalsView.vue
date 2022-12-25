bc1fa48acfa3acbe1bfcf5bb0cb5c6eedcb7f393
coach77777 <108881946+coach77777@users.noreply.github.com>
2022-12-25 13:58:15 -0600

    TeleportModals

---
+++ vue-composition-api-basics/src/views/ModalsView.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="modals">
+  <h1>Modals</h1>
+  <button @click="showModal = true">Show modal</button>
+
+
+
+  //teleport to any named div
+  <teleport to="body">
+    <div v-if ="showModal" class="modal">
+      <h1>This is a modal.</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum iure incidunt ad mollitia, eos, consectetur laudantium impedit in veritatis aperiam excepturi quasi quibusdam repellat natus fugit eius vero unde ea.</p>
+      <button @click="showModal=false">Hide modal</button>
+    </div>
+  </teleport>
+
+</div>
+</template>
+
+<script setup>
+/*
+import
+*/
+import { ref } from 'vue'
+
+
+/*
+modals
+*/
+
+const showModal = ref(false)
+
+</script>
+
+<style>
+.modal {
+  background: beige;
+  padding: 15px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+</style>
