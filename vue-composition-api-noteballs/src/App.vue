894f75ec903d0f0c48dead08c353bda739a84db9
coach77777 <108881946+coach77777@users.noreply.github.com>
2022-12-28 22:11:51 -0600

    BulmaNavBar

--- vue-composition-api-noteballs/src/App.vue
+++ vue-composition-api-noteballs/src/App.vue
@@ -1,14 +1,18 @@
 <template>
-<RouterLink to="/">Notes</RouterLink> | 
-<RouterLink to="/stats">Stats</RouterLink>
+<NavBar />
 
 <RouterView />
 </template>
 
-<script>
+<script setup>
+/*
+imports
+*/
 
+import NavBar from '@/components/Layout/NavBar.vue'
 </script>
 
 <style>
+@import 'bulma\css\bulma.min.css';
 
 </style>
