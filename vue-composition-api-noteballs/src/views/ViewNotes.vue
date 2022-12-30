7eec2aae92b495667b4b369f883320e2d6756abd
coach77777 <108881946+coach77777@users.noreply.github.com>
2022-12-30 15:43:06 -0600

    ChildComponent

--- vue-composition-api-noteballs/src/views/ViewNotes.vue
+++ vue-composition-api-noteballs/src/views/ViewNotes.vue
@@ -25,23 +25,10 @@
   </div>
 </div>
 
- 
-  <div 
-  v-for ="note in notes"
-  :key="note.id"
-  class="card mb-4">
-
-    <div class="card-content">
-      <div class="content">
-  {{ note.content}}
-      </div>
-    </div>
-    <footer class="card-footer">
-
-      <a href="#" class="card-footer-item">Edit</a>
-      <a href="#" class="card-footer-item">Delete</a>
-    </footer>
-  </div>
+ <Note 
+ v-for ="note in notes"
+ :key="note.id"/>
+  
 </div>
 
 </template>
@@ -51,6 +38,7 @@
 Imports
 */
 import { ref } from 'vue'
+import Note from '@/components/Notes/Note.vue'
 
 /*
 notes
