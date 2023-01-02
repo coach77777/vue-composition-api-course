946dabb805ef011afae76b7c8d9cb54da9dab0ad
coach77777 <108881946+coach77777@users.noreply.github.com>
2023-01-02 12:17:08 -0600

    Reusable Component - AddEditNote

--- vue-composition-api-noteballs/src/views/ViewNotes.vue
+++ vue-composition-api-noteballs/src/views/ViewNotes.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="notes">
 
-    <div class="card has-background-success-dark p-4 mb-5">
+    <AddEditNote>
+      <template 
+      v-slot:buttons>
+      <button
+      :disabled="!newNote"
+      @click="addNote"
+      class="button is-link has-background-success"
+    >Add New Note</button>
+
+      </template>
+    </AddEditNote>
+
+    <!-- <div class="card has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
           <textarea
@@ -22,7 +34,7 @@
          >Add New Note</button>
         </div>
       </div>
-    </div>
+    </div> -->
 
     <Note
       v-for="note in storeNotes.notes"
@@ -39,6 +51,7 @@ Imports
 */
 import { ref } from 'vue'
 import Note from '@/components/Notes/Note.vue'
+import AddEditNote from '@/components/Notes/AddEditNote.vue'
 import { useStoreNotes } from '@/stores/storeNotes'
 
 /*
