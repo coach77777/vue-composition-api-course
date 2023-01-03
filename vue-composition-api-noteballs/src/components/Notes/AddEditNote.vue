0d81cbd1ef4f0cb41a8c0a119cf9804950bcd9af
coach77777 <108881946+coach77777@users.noreply.github.com>
2023-01-03 00:14:04 -0600

    Custom Color, Placeholder & Label Props

--- vue-composition-api-noteballs/src/components/Notes/AddEditNote.vue
+++ vue-composition-api-noteballs/src/components/Notes/AddEditNote.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="card has-background-success-dark p-4 mb-5">
+  <div class="card  mb-5"
+  :class="`has-background-${bgColor}-dark p-4`">
+  <label 
+  v-if="label" 
+  class="label has-text-white">
+   {{label}}
+  </label>
+
     <div class="field">
       <div class="control">
         <textarea
@@ -7,7 +14,7 @@
     @input="$emit('update:modelValue', $event.target.value)"
     ref="textareaRef"
     class="textarea"
-    placeholder="Add a new note..."
+    :placeholder="props.placeholder"
 />
       </div>
     </div>
@@ -33,6 +40,17 @@ const props= defineProps({
    modelValue: {
     type: String,
     required: true
+  },
+  bgColor: {
+    type: String,
+    default: 'success'
+  },
+  placeholder:{
+    type: String,
+    default: "Type something ..."
+  },
+  label:{
+    type: String
   }
 })
 
--- vue-composition-api-noteballs/src/views/VieweditNote.vue
+++ vue-composition-api-noteballs/src/views/VieweditNote.vue
@@ -1,11 +1,44 @@
 <template>
 
   <div class="edit-note">
-    <h1>Edit Note with id of : {{ $route.params.id }}</h1>
+    <AddEditNote 
+    v-model="noteContent"
+    bgColor="link"
+    placeholder="Edit Note"
+    label="Edit Note"
+    ref="addEditNoteRef">
+      <template #buttons>
+
+      <RouterLink to="/"
+      class="button is-link is-light"
+    >
+    Cancel
+  </RouterLink>
+  <button
+  class="button is-link has-background-link"
+:disabled="!noteContent"
+>
+Save Note
+</button>
+
+      </template>
+    </AddEditNote>
+
   </div>
 </template>
 
 <script setup>
+
+/*
+imports
+*/
+import { ref } from 'vue';
+import AddEditNote from '@/components/Notes/AddEditNote.vue';
+
+/*
+note
+*/
+const noteContent = ref('')
 
 </script>
 
